2d4fcbe0fa41789b877fe7883beb3ff634db129c
LuanaPBueno <luanabuenorj@gmail.com>
2025-02-10 14:44:57 -0300

    Initial Commit

---
+++ OneStarTests/OneStarTests.swift
@@ -0,0 +1,17 @@
+//
+//  OneStarTests.swift
+//  OneStarTests
+//
+//  Created by Luana Bueno on 10/02/25.
+//
+
+import Testing
+@testable import OneStar
+
+struct OneStarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
